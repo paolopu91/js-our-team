518f228531d88f839f487e43f3a718a21c5dbcd6
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-01 17:16:46 +0200

    add milestone 2 completed

--- js/main.js
+++ js/main.js
@@ -38,7 +38,7 @@ const containerTeam =[
     {
         nome : "Angela Caroll" ,
         ruolo :"Chief Editor" ,
-        foto : "./img/angel-caroll-chief-editor.jpg"
+        foto : "./img/angela-caroll-chief-editor.jpg"
     },
     {
         nome : "Walter Gordon",
@@ -64,4 +64,40 @@ const containerTeam =[
 ];
 console.table(containerTeam);
 
+
 //Milestone 2:
+
+// creo una variabile in cui andrò ad inserire il contenitore delle mie card
+const containerCard = document.querySelector(".team-container");
+
+// creo un ciclo sull'array del mio team
+//per stampare singolarmente ogni collaboratore del mio team da inserire poi nel template
+for(let i = 0; i<containerTeam.length;i++){
+    
+    // creo la mia variabile collaborator per identificare ogni indice del mio containerTeam
+    let collaborator = containerTeam[i];
+    
+    // stampo tutto in console per vedere se funziona
+    console.log(collaborator.nome, collaborator.ruolo,collaborator.foto)
+
+// creo il mio template literal per vedere tutto in html;
+//Ho aggiunto un div con classe col-4 per vedere le immagini incolonnate correttamente in html
+
+const template = `  
+                        <div class="col-4">
+                            <div class="team-card">
+                                <div class="card-image">
+                                    <img src="${collaborator.foto}" alt="Wayne Barnett">
+                                </div>
+                                <div class="card-text">
+                                    <h3>${collaborator.nome}</h3>
+                                    <p>${collaborator.ruolo}</p>
+                                </div>
+                            </div>
+                        </div>
+                    `
+                    
+console.log(template)
+
+containerCard.innerHTML += template
+}
