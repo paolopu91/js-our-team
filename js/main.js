756436ef445309175300c7899b3ac251f5cdb20b
paolopu91 <paolo.pugliese1991@gmail.com>
2022-06-01 16:24:53 +0200

    add changes milestone 1

--- js/main.js
+++ js/main.js
@@ -33,32 +33,32 @@ const containerTeam =[
     {
         nome :"Wayne Barnett" ,
         ruolo : "founder and CEO" ,
-        
+        foto : "./img/wayne-barnett-founder-ceo.jpg"
     },
     {
         nome : "Angela Caroll" ,
         ruolo :"Chief Editor" ,
-        
+        foto : "./img/angel-caroll-chief-editor.jpg"
     },
     {
         nome : "Walter Gordon",
         ruolo : "Office Manager",
-        
+        foto : "./img/walter-gordon-office-manager.jpg"
     },
     {
         nome :"Angela Lopez" ,
         ruolo : "Social Media Manager",
-        
+        foto : "./img/angela-lopez-social-media-manager.jpg"
     },
     {
         nome : "Scott Estrada",
         ruolo :"Developer" ,
-        
+        foto : "./img/scott-estrada-developer.jpg"
     },
     {
         nome : "Barbara Ramos" ,
         ruolo : " Graphic Designer",
-        
+        foto : "./img/barbara-ramos-graphic-designer.jpg"
     },
 
 ];
